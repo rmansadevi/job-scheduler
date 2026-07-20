522b69b68aa227a11f14b76bc9e7dd7175fa0d1e
rmansadevi <rmansadevi@gmail.com>
2026-07-20 11:29:08 +0530

    Initialize spring app

---
+++ src/main/java/com/task/jobscheduler/JobschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.task.jobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/jobscheduler/JobschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.jobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
